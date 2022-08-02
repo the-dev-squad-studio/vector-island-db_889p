db88da6ba3cc9e40d9006e50c2e5baadcc15ba45
tamzid49 <46148776+tamzid49@users.noreply.github.com>
2022-08-02 18:43:34 +0600

    Update patch_v4_1.cs

--- patches/patch_v4_1.cs
+++ patches/patch_v4_1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public class Patch_v4_1 : MonoBehaviour {
     void Start() 
     { 
-        Debug.Log("PATCH WORKS!!"); 
+        Debug.Log("PATCH WORKS!!");
+        IG.System.UI.instance.Notification.NewNotification("Patch has been added");
     }
 }
